79bed9fd37467ca2fe61361bdd6994fb13412365
Rarmash <mrmstitel13@gmail.com>
2025-05-27 17:04:42 +0300

    Added profile mapping

--- src/main/kotlin/com/rarmash/b4cklog_server/controller/UserController.kt
+++ src/main/kotlin/com/rarmash/b4cklog_server/controller/UserController.kt
@@ -3,15 +3,27 @@ package com.rarmash.b4cklog_server.controller
 import org.springframework.web.server.ResponseStatusException
 import org.springframework.http.HttpStatus
 import com.rarmash.b4cklog_server.model.game.GameDAO
+import com.rarmash.b4cklog_server.model.user.User
 import com.rarmash.b4cklog_server.model.user.UserDAO
+import com.rarmash.b4cklog_server.service.AuthService
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/users")
 class UserController(
     private val userDAO: UserDAO,
-    private val gameDAO: GameDAO
+    private val gameDAO: GameDAO,
+    private val authService: AuthService
 ) {
+    @GetMapping("/profile")
+    fun getUserProfile(
+        @RequestHeader("Authorization") authHeader: String
+    ): User {
+        val token = authHeader.removePrefix("Bearer ").trim()
+        val user = authService.getUserByToken(token)
+        return user
+    }
+
     @PostMapping("/{userId}/addGameToList")
     fun addGameToList(
         @PathVariable userId: Int,
